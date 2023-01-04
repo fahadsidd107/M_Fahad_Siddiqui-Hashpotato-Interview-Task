126a615385947a5628c12f2bfcd5c0a0e51f81fb
fahadsidd107 <fsiddiqui107@gmail.com>
2023-01-04 16:02:53 +0500

    wip: left panel list

---
+++ src/components/Left/constant.ts
@@ -0,0 +1,60 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartLine, faSliders } from "@fortawesome/free-solid-svg-icons";
+
+export const upperList = [
+  {
+    icon: faSliders,
+    label: "Dashboard",
+  },
+  {
+    icon: faChartLine,
+    label: "Analytics",
+  },
+  {
+    icon: faChartLine,
+    label: "Stake",
+  },
+  {
+    icon: faChartLine,
+    label: "Band",
+  },
+];
+
+export const lowerList = [
+    {
+        icon: faChartLine,
+        label: "Buy PASS",
+      },
+      {
+        icon: faChartLine,
+        label: "Stake",
+      },  {
+        icon: faChartLine,
+        label: "Stake",
+      }
+  ];
+
+export const bandDiscount =[
+    {
+        coin: "XIV-BNB LP",
+        percentage: "-16.28%",
+      },
+      {
+        coin: "PASS-BNB LP",
+        percentage: "-16.28%",
+      },
+      {
+        coin: faChartLine,
+        percentage: "Stake",
+      },
+      {
+        coin: faChartLine,
+        percentage: "Band",
+      }, 
+]
+
+      // XIV-BNB LP  -16.28%
+      // PASS-BNB LP -16.28%
+      // XIV -16.28%
+      // BNB -16.28%
+      // BUSD -16.28%
--- src/components/Left/menu.tsx
+++ src/components/Left/menu.tsx
@@ -23,11 +23,32 @@ export const items = [
   {
     key: "Band",
     icon: <FontAwesomeIcon icon={faChartLine} />,
-    label: "Stake",
-  },  {
+    label: "Band",
+  },
+    {
     key: "BandDiscount",
     icon: <FontAwesomeIcon icon={faChartLine} />,
     label: "Stake",
+    children: [
+      {
+        key: "users",
+        label: (
+          <div className='list-children'>
+           <p>XIV-BNB LP</p>
+           <p>-16.28%</p>
+          </div>
+        ),
+      },
+
+      {
+        key: "products",
+        label: "Products",
+      },
+      {
+        key: "roles",
+        label: "Roles And Permission",
+      },
+    ],
     // {
       // XIV-BNB LP  -16.28%
       // PASS-BNB LP -16.28%
@@ -39,7 +60,7 @@ export const items = [
   {
     key: "Buy PASS",
     icon: <FontAwesomeIcon icon={faChartLine} />,
-    label: "Stake",
+    label: "Buy PASS",
   },
   {
     key: "Price Chart",
